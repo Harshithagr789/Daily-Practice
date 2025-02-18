4934a65d3d9fb1c8bed933fe1a870531e308b0d9
Harshithagr789 <harshithagr01@gmail.com>
2025-02-18 11:24:11 +0530

    Fifth commit

---
+++ project1/src/project1/Map.java
@@ -0,0 +1,722 @@
+package project1;
+
+public class Map {
+	public static void main(String[] args) {
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=150; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=1; j++) {
+				System.out.print("* ");
+			}
+			for (int j = 1; j <=12; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=2; j++) {
+			System.out.print("* ");
+		}
+			System.out.println();
+	}
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=148; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=3; j++) {
+				System.out.print("* ");
+			}
+			for (int j = 1; j <=9; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j <=1; j++) {
+			System.out.print("* ");
+		}
+			System.out.println();
+	}
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=147; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=4; j++) {
+				System.out.print("* ");
+			}
+			for (int j = 1; j <=7; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j <=1; j++) {
+			System.out.print("* ");
+		}
+			System.out.println();
+	}
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=149; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=5; j++) {
+				System.out.print("* ");
+			}
+			for (int j = 1; j <=3; j++) {
+			System.out.print("* ");
+		}
+			System.out.println();
+	}
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=143; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=10; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	}
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=142; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=10; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	}
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=142; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=8; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=130; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=1; j++) {
+				System.out.print("* ");
+			}
+			for (int j = 1; j <=8; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=8; j++) {
+			System.out.print("* ");
+		}
+			System.out.println();
+	}
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=129; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=2; j++) {
+				System.out.print("* ");
+			}
+			for (int j = 1; j <=3; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=2; j++) {
+				System.out.print("* ");
+			}
+			for (int j = 1; j <=2; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=5; j++) {
+			System.out.print("* ");
+		}
+			System.out.println();
+	}
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=129; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=9; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=122; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=10; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=108; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=15; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=104; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=15; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=104; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=12; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=106; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=9; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=103; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=9; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=100; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=10; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=97; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=12; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=94; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=2; j++) {
+				System.out.print("* ");
+			}
+			for (int j = 1; j <=2; j++) {
+				System.out.print(" ");
+			}
+			
+			for (int j = 1; j <=11; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=95; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=13; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=85; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=17; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=82; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=17; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=82; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=14; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=78; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=16; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=76; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=16; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=68; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=3; j++) {
+				System.out.print("* ");
+			}
+			for (int j = 1; j <=1; j++) {
+				System.out.print(" ");
+			}	
+			for (int j = 1; j <=16; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=41; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=7; j++) {
+				System.out.print("* ");
+			}
+			for (int j = 1; j <=5; j++) {
+				System.out.print(" ");
+			}	
+			for (int j = 1; j <=23; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=41; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=31; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=44; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=24; j++) {
+				System.out.print("* ");
+			}
+			for (int j = 1; j <=6; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=3; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=40; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=25; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=40; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=25; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=41; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=24; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=36; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=2; j++) {
+				System.out.print("* ");
+			}
+			for (int j = 1; j <=3; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=23; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=36; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=26; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=38; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=26; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=36; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=28; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=35; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=29; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=35; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=28; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=37; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=27; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=36; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=28; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=37; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=27; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=33; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=2; j++) {
+				System.out.print("* ");
+			}
+			for (int j = 1; j <=3; j++) {
+				System.out.print(" ");
+			}	
+			for (int j = 1; j <=25; j++) {
+				System.out.print("* ");
+			}
+			
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=34; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=1; j++) {
+				System.out.print("* ");
+			}	
+			for (int j = 1; j <=28; j++) {
+				System.out.print("* ");
+			}	
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=33; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=29; j++) {
+				System.out.print("* ");
+			}	
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=33; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=2; j++) {
+				System.out.print("* ");
+			}	
+			for (int j = 1; j <=28; j++) {
+				System.out.print("* ");
+			}	
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=32; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=2; j++) {
+				System.out.print("* ");
+			}
+			for (int j = 1; j <=4; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=3; j++) {
+				System.out.print("* ");
+			}
+			for (int j = 1; j <=5; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=22; j++) {
+				System.out.print("* ");
+			}	
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=40; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=1; j++) {
+				System.out.print("* ");
+			}
+			for (int j = 1; j <=9; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=22; j++) {
+				System.out.print("* ");
+			}	
+			System.out.println();
+	  }
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=49; j++) {
+				System.out.print(" ");
+			}
+		for (int j = 1; j <=22; j++) {
+			System.out.print("* ");
+		}
+		for (int j = 1; j <=2; j++) {
+			System.out.print(" ");
+		}
+		for (int j = 1; j <=1; j++) {
+			System.out.print("* ");
+		}
+		System.out.println();
+	}
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=50; j++) {
+				System.out.print(" ");
+			}
+		for (int j = 1; j <=21; j++) {
+			System.out.print("* ");
+		}
+		for (int j = 1; j <=2; j++) {
+			System.out.print(" ");
+		}
+		for (int j = 1; j <=1; j++) {
+			System.out.print("* ");
+		}
+		for (int j = 1; j <=1; j++) {
+			System.out.print(" ");
+		}
+		for (int j = 1; j <=1; j++) {
+			System.out.print("* ");
+		}
+		System.out.println();
+	}
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=54; j++) {
+				System.out.print(" ");
+			}
+		for (int j = 1; j <=20; j++) {
+			System.out.print("* ");
+		}
+		for (int j = 1; j <=2; j++) {
+			System.out.print(" ");
+		}
+		for (int j = 1; j <=1; j++) {
+			System.out.print("* ");
+		}
+		for (int j = 1; j <=1; j++) {
+			System.out.print(" ");
+		}
+		for (int j = 1; j <=1; j++) {
+			System.out.print("* ");
+		}
+		System.out.println();
+	}
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=54; j++) {
+				System.out.print(" ");
+			}
+		for (int j = 1; j <=18; j++) {
+			System.out.print("* ");
+		}
+		for (int j = 1; j <=7; j++) {
+			System.out.print(" ");
+		}
+		for (int j = 1; j <=1; j++) {
+			System.out.print("* ");
+		}
+		System.out.println();
+	}
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=57; j++) {
+				System.out.print(" ");
+			}
+		for (int j = 1; j <=16; j++) {
+			System.out.print("* ");
+		}
+		System.out.println();
+	}
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=57; j++) {
+				System.out.print(" ");
+			}
+		for (int j = 1; j <=9; j++) {
+			System.out.print("* ");
+		}
+		for (int j = 1; j <=3; j++) {
+			System.out.print(" ");
+		}
+		for (int j = 1; j <=4; j++) {
+			System.out.print("* ");
+		}
+		System.out.println();
+	}
+		for (int i = 1; i<=1; i++) {
+			for (int j = 0; j <=57; j++) {
+				System.out.print(" ");
+			}
+		for (int j = 1; j <=9; j++) {
+			System.out.print("* ");
+		}
+		for (int j = 1; j <=5; j++) {
+			System.out.print(" ");
+		}
+		for (int j = 1; j <=2; j++) {
+			System.out.print("* ");
+		}
+		System.out.println();
+	}
+		for (int i = 1; i <=1; i++) {
+			for (int j = 1; j <=57; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=3; j++) {
+				System.out.print("* ");
+			}
+		System.out.println();
+	}
+		for (int i = 1; i <=1; i++) {
+			for (int j = 1; j <=55; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=3; j++) {
+				System.out.print("* ");
+			}
+		System.out.println();
+	}
+		for (int i = 1; i <=1; i++) {
+			for (int j = 1; j <=52; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=5; j++) {
+				System.out.print("* ");
+			}
+		System.out.println();
+	}
+		for (int i = 1; i <=1; i++) {
+			for (int j = 1; j <=51; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=5; j++) {
+				System.out.print("* ");
+			}
+		System.out.println();
+	}
+		for (int i = 1; i <=1; i++) {
+			for (int j = 1; j <=54; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <=3; j++) {
+				System.out.print("* ");
+			}
+		System.out.println();
+	}
+	}
+}
